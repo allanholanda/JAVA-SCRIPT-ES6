1b4cb3f2a0aa3cd01dae3b6baf9e1db2db9d5f86
Allan Holanda <allanalvesdeholanda@gmail.com>
2021-10-06 23:18:56 -0300

    Estrutura-Repetição

---
+++ repeticao/1-for.js
@@ -0,0 +1,13 @@
+/** 
+
+for([expressaoInicial]); [condicao]; [incremento])
+    declaração
+
+*/
+
+const array = ['pne', 'two', 'three'];
+
+for(let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log('Element' (index)  (element);
+}
